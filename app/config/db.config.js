ad73e9b9606c9242d1dd769ad689cfb192800080
Hampton Alford <53918599+hs-alford@users.noreply.github.com>
2023-06-24 14:54:28 -0400

    Update db.config.js

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,17 +1,7 @@
 module.exports = {
     HOST: "localhost",
     USER: "root",
-    PASSWORD: "Bassist15$",
-    DB: "firstcapitalrestoration_",
+    PASSWORD: "yourpassword",
+    DB: "dbnamehere",
     dialect: "mysql"
   };
-
-/*
-  module.exports = {
-    HOST: "mysql.firstcapitalrestoration.com",
-    USER: "firstcapitalrest",
-    PASSWORD: "uCd7ixZ5",
-    DB: "firstcapitalrestoration_",
-    dialect: "mysql"
-  };
-  */
